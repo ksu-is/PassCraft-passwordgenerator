598cb1112e8f870c797c53b89b6a685ef9b28781
chasemiller04 <134944322+chasemiller04@users.noreply.github.com>
2024-04-01 09:26:03 -0400

    Commit #2
    Commit #2 starts just by adding a new string to welcome the users. In the while True loop function, I wanted this program to give however many passwords the user wants with the 'num_passwords'. Within the loop function, the 'try' will break any attempt to turn into an integer. It will also print the statement "Please enter a valid integer," if the user does not give a valid integer.

--- PassCraft.py
+++ PassCraft.py
@@ -57,3 +57,16 @@ def generate_password(length, choices):
             characterList += string.punctuation
     password = [random.choice(characterList) for _ in range(length)]
     return ''.join(password)
+
+def main():
+    print("Welcome to Pass Craft Password Generator!")
+
+    while True:
+        try:
+            num_passwords = int(input("How many passwords would you like to generate? "))
+            if num_passwords <= 0:
+                print("Please enter a positive integer.")
+            else:
+                break
+        except ValueError:
+            print("Please enter a valid integer.")
